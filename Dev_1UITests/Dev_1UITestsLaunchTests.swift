15235743151516985f2a9e50a05f4572120b392d
Dmitry <apostate3@gmail.com>
2023-06-19 17:35:18 +0300

    Init

---
+++ Dev_1UITests/Dev_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dev_1UITestsLaunchTests.swift
+//  Dev_1UITests
+//
+//  Created by Dmitry on 19.06.2023.
+//
+
+import XCTest
+
+final class Dev_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
